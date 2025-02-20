70919434cd2a4c4068a98aec83aa4c75973f84ee
diegoatl11 <diegoatl1102@gmail.com>
2025-02-19 20:35:22 -0500

    datafur: navbar y home

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "./styles/index.css";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
     <App />
-  </StrictMode>,
-)
+  </BrowserRouter>
+);
---
+++ src/pages/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Navbar.css";
+const Navbar = () => {
+  return (
+    <>
+      <nav className="container-primary">
+        <a class="title-navbar" href="#">
+          Panel de Administracion
+        </a>
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
